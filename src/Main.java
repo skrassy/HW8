234c277ea6e4dc861e91b646a85db7d6862c601d
Skrassy NB <skrassy@mail.ru>
2021-06-17 13:13:41 +0300

    Homework 8 *** (в консоли) Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"}; При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя, сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы которые стоят на своих местах. apple – загаданное apricot - ответ игрока ap############# (15 символов, чтобы пользователь не мог узнать длину слова) Для сравнения двух слов посимвольно, можно пользоваться: String str = "apple"; str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции Играем до тех пор, пока игрок не отгадает слово Используем только маленькие буквы

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Компьютер загадал слово. Угадайте его.");
+        Game game = new Game();
+        game.start();
+    }
+}
